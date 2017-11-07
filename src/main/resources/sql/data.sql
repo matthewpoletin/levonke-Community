9f765fd1aa647b7c2108cf51787e03933a82e072
matthewpoletin <contact@matthewpoletin.ru>
2017-11-07 19:36:16 +0300

    Changed data accordingly

--- src/main/resources/sql/data.sql
+++ src/main/resources/sql/data.sql
@@ -1,11 +1,11 @@
-INSERT INTO community.users(users_username, users_forename, users_surname, users_regemail, users_pubemail, users_ghlink, users_fbLink)
-    VALUES ('MatthewPoletin', 'Matthew', 'Poletin', 'poletinm@yandex.ru', 'contact@matthewpoletin.ru', 'github.com/matthewpoletin', 'fb.me/matthewpoletin');
-INSERT INTO community.users(users_username, users_forename, users_surname, users_regemail, users_pubemail, users_ghlink, users_fbLink)
-    VALUES ('kormvina', 'Ksenia', 'Strogina', 'ksyshkazaglushka@mail.ru', 'stroginakp@student.bmstu.ru', 'github.com/kormvina', 'fb.me/kseniastrogina');
-INSERT INTO community.users(users_username, users_forename, users_surname, users_regemail, users_pubemail, users_ghlink, users_fbLink)
-	VALUES ('molros', 'Ogneslav', 'Pestritski', 'persicor@gmail.com', 'persicor@gmail.com', 'github.com/persicor', 'fb.me/persicor');
-INSERT INTO community.users(users_username, users_forename, users_surname, users_regemail, users_pubemail, users_ghlink, users_fbLink)
-	VALUES ('sworen', 'Isaac', 'Grey', 'persicor@gmail.com', 'persicor@gmail.com', 'github.com/isaac_grey', 'fb.me/isaacg');
+INSERT INTO community.users(users_username, users_password, users_forename, users_surname, users_regemail, users_pubemail, users_ghlink, users_fbLink)
+    VALUES ('MatthewPoletin', '202cb962ac59075b964b07152d234b70', 'Matthew', 'Poletin', 'poletinm@yandex.ru', 'contact@matthewpoletin.ru', 'github.com/matthewpoletin', 'fb.me/matthewpoletin');
+INSERT INTO community.users(users_username, users_password, users_forename, users_surname, users_regemail, users_pubemail, users_ghlink, users_fbLink)
+    VALUES ('kormvina', 'd8578edf8458ce06fbc5bb76a58c5ca4', 'Ksenia', 'Strogina', 'ksyshkazaglushka@mail.ru', 'stroginakp@student.bmstu.ru', 'github.com/kormvina', 'fb.me/kseniastrogina');
+INSERT INTO community.users(users_username, users_password, users_forename, users_surname, users_regemail, users_pubemail, users_ghlink, users_fbLink)
+	VALUES ('molros', '5f4dcc3b5aa765d61d8327deb882cf99', 'Ogneslav', 'Pestritski', 'persicor@gmail.com', 'persicor@gmail.com', 'github.com/persicor', 'fb.me/persicor');
+INSERT INTO community.users(users_username, users_password, users_forename, users_surname, users_regemail, users_pubemail, users_ghlink, users_fbLink)
+	VALUES ('sworen', '1a1dc91c907325c69271ddf0c944bc72', 'Isaac', 'Grey', 'persicor@gmail.com', 'persicor@gmail.com', 'github.com/isaac_grey', 'fb.me/isaacg');
 
 INSERT INTO community.organizations(organizations_name, organizations_description, organizations_pubemail, organizations_website, organizations_owner_id)
 	VALUES ('Haywire Corp.', 'Wearable technologies', 'ask@haywire.ru', 'haywire.ru', (SELECT users_name FROM community.users WHERE users_username = 'MatthewPoletin'));

3398c2166a44eb2a6a44ca04d7febd70f03e0fb7
matthewpoletin <contact@matthewpoletin.ru>
2017-11-14 22:22:03 +0300

    Halfway there with UserController tests

--- src/test/java/com/levonke/Community/web/UserControllerTest.java
+++ src/test/java/com/levonke/Community/web/UserControllerTest.java
@@ -1,29 +1,39 @@
 package com.levonke.Community.web;
 
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.json.UTF8JsonGenerator;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.levonke.Community.domain.User;
 import com.levonke.Community.repository.UserRepository;
 import com.levonke.Community.service.UserServiceImpl;
 import com.levonke.Community.web.model.UserRequest;
 import com.levonke.Community.web.model.UserResponse;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
-import static org.hamcrest.core.IsEqual.equalTo;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThat;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
 import java.util.ArrayList;
 import java.util.Optional;
 
+@SpringBootTest
+@AutoConfigureMockMvc
 @ExtendWith(SpringExtension.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 @DisplayName("UserController Test")
 class UserControllerTest {
 	
@@ -39,7 +49,13 @@ class UserControllerTest {
 		.setGhLink("github.com/username");
 	
 	@Autowired
-	private TestRestTemplate restTemplate;
+	ObjectMapper objectMapper;
+	
+	@Autowired
+	private WebApplicationContext wac;
+	
+	@Autowired
+	private MockMvc mockMvc;
 	
 	@InjectMocks
 	private UserServiceImpl userService;
@@ -47,64 +63,86 @@ class UserControllerTest {
 	@MockBean
 	private UserRepository userRepositoryMock;
 	
+	@BeforeAll
+	void setUpMockMvc(WebApplicationContext wac) {
+		mockMvc = webAppContextSetup(wac)
+			.build();
+	}
+	
 	@Test
 	@DisplayName("Create user")
-	void createUser() {
+	void createUser() throws Exception {
 		User userNoId = new User()
-			.setUsername("Username")
-			.setPassword("Password")
-			.setForename("Forename")
-			.setSurname("Surname")
-			.setRegEmail("name@server.domain")
-			.setPubEmail("name@server.domain")
-			.setFbLink("fb.com/username")
-			.setGhLink("github.com/username")
-			.setOrganizations(new ArrayList<>())
-			.setTeams(new ArrayList<>());
-
+				.setUsername("Username")
+				.setPassword("Password")
+				.setForename("Forename")
+				.setSurname("Surname")
+				.setRegEmail("name@server.domain")
+				.setPubEmail("name@server.domain")
+				.setFbLink("fb.com/username")
+				.setGhLink("github.com/username")
+				.setOrganizations(new ArrayList<>())
+				.setTeams(new ArrayList<>());
+		
 		when(userRepositoryMock.save(userNoId)).thenReturn(user);
-
+		
 		UserRequest userRequest = new UserRequest()
-			.setUsername("Username")
-			.setPassword("Password")
-			.setForename("Forename")
-			.setSurname("Surname")
-			.setRegEmail("name@server.domain")
-			.setPubEmail("name@server.domain")
-			.setFbLink("fb.com/username")
-			.setGhLink("github.com/username");
-
+				.setUsername("Username")
+				.setPassword("Password")
+				.setForename("Forename")
+				.setSurname("Surname")
+				.setRegEmail("name@server.domain")
+				.setPubEmail("name@server.domain")
+				.setFbLink("fb.com/username")
+				.setGhLink("github.com/username");
+		
 		UserResponse expectedResponse = new UserResponse(user);
-		UserResponse actualResponse = restTemplate.postForObject("/api/community/users", userRequest, UserResponse.class);
-
+		
+		MvcResult result = this.mockMvc.perform(
+				post(UserController.userBaseURI)
+				.content(new ObjectMapper().writeValueAsString(userRequest))
+				.contentType(MediaType.APPLICATION_JSON)
+				).andExpect(status().isCreated())
+				.andReturn();
+		UserResponse actualResponse = new ObjectMapper().readValue(result.getResponse().getContentAsString(), new TypeReference<UserResponse>() {
+		});
+		
 		verify(userRepositoryMock, times(1)).save(userNoId);
-		assertThat("Invalid user response", expectedResponse, equalTo(actualResponse));
+		assertEquals("Invalid country response", expectedResponse, actualResponse);
 	}
 	
 	@Test
 	@DisplayName("Get user")
-	void getUser() {
+	void getUser() throws Exception {
 		Optional<User> userOptional = Optional.of(user);
-
+		
 		when(userRepositoryMock.findById(1)).thenReturn(userOptional);
-
+		
+		MvcResult result = this.mockMvc.perform(
+			get(UserController.userBaseURI + "/1"))
+			.andExpect(status().is2xxSuccessful()
+			).andReturn();
+		
 		UserResponse expectedResponse = new UserResponse(user);
-		UserResponse actualResponse = restTemplate.getForObject("/api/community/users/1", UserResponse.class);
-
+		UserResponse actualResponse = new ObjectMapper().readValue(result.getResponse().getContentAsString(), new TypeReference<UserResponse>() { });
+		
 		verify(userRepositoryMock, times(1)).findById(1);
-		assertThat("Should return valid user", actualResponse, equalTo(expectedResponse));
+		assertEquals("Invalid country response", expectedResponse, actualResponse);
 	}
 	
+	// TODO: implement
+	@Disabled
 	@Test
 	@DisplayName("Update user")
-	void updateUser() {
-	
+	void updateUser() throws Exception {
 	}
 	
 	@Test
 	@DisplayName("Delete user")
-	void deleteUser() {
-		restTemplate.delete("/api/community/users/1");
+	void deleteUser() throws Exception {
+		this.mockMvc.perform(
+			delete(UserController.userBaseURI + "/1")
+			).andExpect(status().isNoContent());
 		
 		verify(userRepositoryMock, times(1)).deleteById(1);
 	}
